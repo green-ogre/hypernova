90d1ede535a572773ae4b7b03d952c65f612fe3d
green-ogre <balln13572@gmail.com>
2024-09-01 23:56:20 -0600

    shapes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use bevy::{
     input::{keyboard::KeyboardInput, ButtonState},
     prelude::*,
+    render::{
+        mesh::{Indices, Mesh, PrimitiveTopology},
+        render_asset::RenderAssetUsages,
+    },
 };
 
 fn main() {
@@ -14,6 +18,7 @@ fn main() {
             ..Default::default()
         }))
         .add_systems(Update, exit_on_esc)
+        .add_systems(Startup, setup)
         .run();
 }
 
@@ -23,4 +28,54 @@ fn exit_on_esc(mut reader: EventReader<KeyboardInput>, mut writer: EventWriter<A
             writer.send(AppExit::Success);
         }
     }
+}
+
+fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>) {
+    let mesh = build_circle(50., 4);
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(ColorMesh2dBundle {
+        mesh: meshes.add(mesh).into(),
+        transform: Transform::from_translation(Vec3::new(0., 0., 0.)),
+        ..Default::default()
+    });
+}
+
+const PI2: f32 = std::f32::consts::PI * 2.0;
+
+pub fn build_circle(radius: f32, vertices: usize) -> Mesh {
+    let mut mesh = Mesh::new(
+        PrimitiveTopology::TriangleList,
+        RenderAssetUsages::RENDER_WORLD,
+    );
+    let n_vertices = vertices + 1;
+    let n_triangles = vertices as u32;
+    let mut positions: Vec<[f32; 3]> = Vec::with_capacity(n_vertices);
+    let mut normals: Vec<[f32; 3]> = Vec::with_capacity(n_vertices);
+    let mut uvs: Vec<[f32; 2]> = Vec::with_capacity(n_vertices);
+    let mut indices: Vec<u32> = Vec::with_capacity((n_triangles as usize) * 3);
+    for i in 0..vertices {
+        let angle = (i as f32) / (vertices as f32) * PI2;
+        let c = angle.cos();
+        let s = angle.sin();
+        let x = radius * c;
+        let y = radius * s;
+        let u = 0.5 * c + 0.5;
+        let v = -0.5 * s + 0.5;
+        positions.push([x, y, 0.]);
+        normals.push([0., 0., 1.]);
+        uvs.push([u, v]);
+    }
+    positions.push([0., 0., 0.]);
+    normals.push([0., 0., 1.]);
+    uvs.push([0.5, 0.5]);
+    for i in 0..n_triangles {
+        indices.push(i % n_triangles);
+        indices.push((i + 1) % n_triangles);
+        indices.push(n_triangles);
+    }
+    mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, positions);
+    mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
+    mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, uvs);
+    mesh.insert_indices(Indices::U32(indices));
+    mesh
 }
