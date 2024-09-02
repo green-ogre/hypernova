e6f1bc2e2c61236061c4e6e2505f52219980c9e4
green-ogre <balln13572@gmail.com>
2024-09-01 23:04:12 -0600

    bevy init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use bevy::{input::{keyboard::KeyboardInput, ButtonState}, prelude::*};
+
 fn main() {
-    println!("Hello, world!");
+    App::default().add_plugins(DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(Window {
+        title: "hypernova".into(),
+        resolution: [1920., 1080.].into(),
+        ..Default::default()
+        }),
+        ..Default::default()
+    }))
+        .add_systems(Update, exit_on_esc)
+        .run();
+}
+
+fn exit_on_esc(mut reader: EventReader<KeyboardInput>, mut writer: EventWriter<AppExit>) {
+    for input in reader.read() {
+        if matches!(input, KeyboardInput { state, key_code, .. } if *state == ButtonState::Pressed && *key_code == KeyCode::Escape) {
+        writer.send(AppExit::Success);
+        }
+    }
 }
