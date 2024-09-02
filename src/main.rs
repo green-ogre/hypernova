671c8d0d3ac15450cedc5b2aed1790d54b780a93
CorvusPrudens <corvusprudens@gmail.com>
2024-09-01 23:15:54 -0600

    Added dynamic linking and stuff

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,26 @@
-use bevy::{input::{keyboard::KeyboardInput, ButtonState}, prelude::*};
+use bevy::{
+    input::{keyboard::KeyboardInput, ButtonState},
+    prelude::*,
+};
 
 fn main() {
-    App::default().add_plugins(DefaultPlugins.set(WindowPlugin {
-        primary_window: Some(Window {
-        title: "hypernova".into(),
-        resolution: [1920., 1080.].into(),
-        ..Default::default()
-        }),
-        ..Default::default()
-    }))
+    App::default()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "hypernova".into(),
+                resolution: [1920., 1080.].into(),
+                ..Default::default()
+            }),
+            ..Default::default()
+        }))
         .add_systems(Update, exit_on_esc)
         .run();
 }
 
 fn exit_on_esc(mut reader: EventReader<KeyboardInput>, mut writer: EventWriter<AppExit>) {
     for input in reader.read() {
-        if matches!(input, KeyboardInput { state, key_code, .. } if *state == ButtonState::Pressed && *key_code == KeyCode::Escape) {
-        writer.send(AppExit::Success);
+        if input.state == ButtonState::Pressed && input.key_code == KeyCode::Escape {
+            writer.send(AppExit::Success);
         }
     }
 }
