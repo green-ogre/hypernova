646aac0d5d3d4e09d449a90425f83ecb25d5ba97
green-ogre <balln13572@gmail.com>
2024-09-02 14:18:41 -0600

    shootin bullets and shit

---
+++ src/camera.rs
@@ -0,0 +1,226 @@
+use bevy::prelude::*;
+use noisy_bevy::simplex_noise_2d;
+use rand::{Rng, SeedableRng};
+use std::f32::consts::TAU;
+
+use crate::{MainCamera, Player};
+
+#[derive(Debug)]
+pub struct CameraPlugin;
+
+impl Plugin for CameraPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(PlayerCamera::default())
+            .add_systems(Startup, startup);
+        // .egui_resource::<ScreenShake>()
+        // .insert_resource(ScreenShake {
+        //     intensity: 42.0,
+        //     duration: 0.5,
+        //     start_time: 0.0,
+        // })
+        // .add_systems(Schedule::Update, shake_screen)
+        // .add_systems(PostUpdate, update_camera);
+    }
+}
+
+fn startup(mut commands: Commands) {
+    commands.spawn((MainCamera, Camera2dBundle::default()));
+}
+
+#[derive(Resource, Default, Debug, Clone, Copy)]
+pub struct ScreenShake {
+    intensity: f32,
+    duration: f32,
+    start_time: f32,
+    x_offset: Vec2,
+    y_offset: Vec2,
+}
+
+impl ScreenShake {
+    pub fn new(intensity: f32, duration: f32, start_time: f32) -> Self {
+        let mut rng = rand::rngs::SmallRng::from_entropy();
+        let mut offset = move || {
+            Vec2::new(
+                rng.gen_range(-1000f32..1000f32),
+                rng.gen_range(-1000f32..1000f32),
+            )
+        };
+
+        Self {
+            intensity,
+            duration,
+            start_time,
+            x_offset: offset(),
+            y_offset: offset(),
+        }
+    }
+}
+
+// fn shake_screen(
+//     reader: EventReader<KeyInput>,
+//     mut camera: ResMut<PlayerCamera>,
+//     shake: Res<ScreenShake>,
+//     delta: Res<DeltaTime>,
+// ) {
+//     for event in reader.peak_read() {
+//         if matches!(
+//             event,
+//             KeyInput {
+//                 code: KeyCode::I,
+//                 state: KeyState::Pressed,
+//                 ..
+//             }
+//         ) {
+//             let mut shake = *shake;
+//             shake.start_time = delta.wrapping_elapsed_as_seconds();
+//             camera.push_screen_shake(shake)
+//         }
+//     }
+// }
+
+fn update_camera(
+    mut player_camera: ResMut<PlayerCamera>,
+    player: Query<&Transform, With<Player>>,
+    mut camera: Query<&mut Transform, (With<MainCamera>, Without<Player>)>,
+    time: Res<Time>,
+    // mouse_position: Res<MousePosition>,
+) {
+    let Ok(mut camera) = camera.get_single_mut() else {
+        return;
+    };
+
+    let Ok(player) = player.get_single() else {
+        return;
+    };
+
+    player_camera.apply_screen_shake(&time);
+    player_camera.follow_player(
+        player,
+        // vel,
+        &mut camera, //&mouse_position,
+        &time,
+    );
+}
+
+/// Handles main camera logic.
+#[derive(Resource)]
+pub struct PlayerCamera {
+    screen_shake: Vec<ScreenShake>,
+    max_smooth_factor: f32,
+    min_smooth_factor: f32,
+    max_distance: f32,
+    delta: f32,
+    lead_factor: f32,
+    shake_offset: Vec3,
+    follow_point: Vec3,
+    // noise: Noise,
+}
+
+// #[derive(Debug)]
+// pub struct Noise(noise::OpenSimplex);
+//
+// impl egui_widget::Widget for Noise {
+//     fn display(&mut self, ui: &mut egui::Ui) {}
+// }
+
+impl Default for PlayerCamera {
+    fn default() -> Self {
+        Self {
+            screen_shake: Vec::new(),
+            max_smooth_factor: 1.0,
+            min_smooth_factor: 1.0,
+            max_distance: 100.0,
+            delta: 10.0,
+            lead_factor: 1.0,
+            follow_point: Vec3::new(0., 0., 0.),
+            shake_offset: Vec3::ZERO,
+            // noise: Noise(noise::OpenSimplex::new(1)),
+        }
+    }
+}
+
+impl PlayerCamera {
+    pub fn follow_player(
+        &mut self,
+        // velocity: &DirectionalVelocity,
+        player: &Transform,
+        camera: &mut Transform,
+        // mouse_position: &MousePosition,
+        dt: &Time,
+    ) {
+        // let direction: Vec3 = mouse_position.0.into();
+        // let offset = (direction - player.translation).normalize() * 100.;
+        let offset = 0.;
+
+        let target = player.translation + offset;
+
+        let distance_to_target = (target - self.follow_point).length();
+
+        let target: Vec3 = target; // Vec3::from(*velocity).normalize() + target;
+
+        if distance_to_target < self.delta {
+            self.follow_point = target;
+        } else {
+            // Calculate a dynamic smooth factor based on the distance
+            let smooth_factor = if distance_to_target > self.max_distance {
+                self.max_smooth_factor
+            } else {
+                let t = distance_to_target / self.max_distance;
+                self.min_smooth_factor + (self.max_smooth_factor - self.min_smooth_factor) * t
+            };
+
+            // Move the camera smoothly towards the target position
+            self.follow_point += (target - self.follow_point) * smooth_factor * dt.delta_seconds();
+        }
+
+        camera.translation = self.translation();
+    }
+
+    pub fn push_screen_shake(&mut self, shake: ScreenShake) {
+        self.screen_shake.push(shake);
+    }
+
+    pub fn push_screen_shake_with(&mut self, intensity: f32, duration: f32, start_time: f32) {
+        self.screen_shake
+            .push(ScreenShake::new(intensity, duration, start_time));
+    }
+
+    fn apply_screen_shake(&mut self, dt: &Time) {
+        for shake in self.screen_shake.iter_mut() {
+            let elapsed = dt.elapsed_seconds_wrapped() - shake.start_time;
+            if elapsed < shake.duration {
+                let remaining = 1.0 - (elapsed / shake.duration);
+                let current_intensity = shake.intensity * remaining;
+
+                // Sample the simplex space in a circle
+                let radius = 5.;
+
+                let x_noise = simplex_noise_2d(Vec2::new(
+                    shake.x_offset.x + (remaining * TAU).cos() * radius,
+                    shake.x_offset.y + (remaining * TAU).sin() * radius,
+                ));
+
+                let y_noise = simplex_noise_2d(Vec2::new(
+                    shake.y_offset.x + (remaining * TAU).cos() * radius,
+                    shake.y_offset.y + (remaining * TAU).sin() * radius,
+                ));
+
+                self.shake_offset = Vec3::new(
+                    x_noise as f32 * current_intensity,
+                    y_noise as f32 * current_intensity,
+                    0.0, // typically, we don't shake on the z-axis
+                );
+            } else {
+                // Shake finished
+                self.shake_offset = Vec3::new(0.0, 0.0, 0.0);
+                shake.start_time = 0.0;
+            }
+        }
+
+        self.screen_shake.retain(|s| s.start_time > 0.0);
+    }
+
+    fn translation(&self) -> Vec3 {
+        self.follow_point + self.shake_offset
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 use bevy::{
     input::{keyboard::KeyboardInput, ButtonState},
+    math::VectorSpace,
     prelude::*,
+    reflect::List,
     render::{
+        camera::CameraPlugin,
         mesh::{Indices, Mesh, PrimitiveTopology},
         render_asset::RenderAssetUsages,
     },
+    sprite::Mesh2dHandle,
+    window::{PresentMode, PrimaryWindow},
 };
+use camera::PlayerCamera;
+use iyes_perf_ui::{entries::PerfUiBundle, PerfUiPlugin};
+use leafwing_input_manager::prelude::*;
+use rand::Rng;
+use std::f32::consts::PI;
+
+mod camera;
 
 fn main() {
     App::default()
@@ -13,14 +25,67 @@ fn main() {
             primary_window: Some(Window {
                 title: "hypernova".into(),
                 resolution: [1920., 1080.].into(),
+                present_mode: PresentMode::Immediate,
                 ..Default::default()
             }),
             ..Default::default()
         }))
-        .add_systems(Update, exit_on_esc)
-        .add_systems(Startup, setup)
+        .add_plugins((
+            PerfUiPlugin,
+            bevy::diagnostic::SystemInformationDiagnosticsPlugin,
+            bevy::diagnostic::EntityCountDiagnosticsPlugin,
+            bevy::diagnostic::FrameTimeDiagnosticsPlugin,
+        ))
+        .add_plugins((
+            InputManagerPlugin::<MoveAction>::default(),
+            InputManagerPlugin::<FireAction>::default(),
+        ))
+        .add_plugins(camera::CameraPlugin)
+        .add_systems(
+            Startup,
+            (setup, spawn_player, spawn_enemy, build_bullet_mesh),
+        )
+        .add_systems(
+            Update,
+            (
+                exit_on_esc,
+                move_player,
+                fire_bullets,
+                update_health_bar,
+                bullet_hit_enemy,
+                despawn_with_no_health,
+            )
+                .chain(),
+        )
+        .add_systems(FixedPostUpdate, (apply_friction, update_velocity))
         .run();
 }
+
+#[derive(Component)]
+struct Health {
+    current: f32,
+    max: f32,
+}
+
+impl Health {
+    pub fn from_max(max: f32) -> Self {
+        Self { max, current: max }
+    }
+}
+
+#[derive(Component)]
+struct HealthBar;
+
+#[derive(Debug, Actionlike, PartialEq, Eq, Hash, Clone, Copy, Reflect)]
+enum MoveAction {
+    Left,
+    Right,
+    Up,
+    Down,
+}
+
+#[derive(Debug, Actionlike, PartialEq, Eq, Hash, Clone, Copy, Reflect)]
+struct FireAction;
 
 fn exit_on_esc(mut reader: EventReader<KeyboardInput>, mut writer: EventWriter<AppExit>) {
     for input in reader.read() {
@@ -30,19 +95,262 @@ fn exit_on_esc(mut reader: EventReader<KeyboardInput>, mut writer: EventWriter<A
     }
 }
 
-fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>) {
-    let mesh = build_circle(50., 4);
-    commands.spawn(Camera2dBundle::default());
-    commands.spawn(ColorMesh2dBundle {
-        mesh: meshes.add(mesh).into(),
-        transform: Transform::from_translation(Vec3::new(0., 0., 0.)),
-        ..Default::default()
+#[derive(Component)]
+struct Player;
+
+#[derive(Component)]
+struct Velocity(Vec3);
+
+#[derive(Component)]
+struct Friction(f32);
+
+impl Velocity {
+    pub fn add_velocity_clamped(&mut self, velocity: Vec3, max_len: f32) {
+        self.0 += velocity;
+        self.0 = self.0.clamp_length(0., max_len);
+    }
+}
+
+fn apply_friction(mut entities: Query<(&mut Velocity, &Friction)>, time: Res<Time>) {
+    for (mut velocity, friction) in entities.iter_mut() {
+        let dir = velocity.0.normalize_or_zero();
+        let friction =
+            (dir * friction.0 * time.delta_seconds()).clamp_length_max(velocity.0.length());
+        velocity.0 -= friction;
+    }
+}
+
+#[derive(Component)]
+struct Enemy;
+
+#[derive(Component)]
+struct MainCamera;
+
+fn setup(mut commands: Commands) {
+    commands.spawn(PerfUiBundle::default());
+}
+
+fn spawn_player(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>) {
+    let move_input_map = InputMap::new([
+        (MoveAction::Left, KeyCode::KeyA),
+        (MoveAction::Right, KeyCode::KeyD),
+        (MoveAction::Up, KeyCode::KeyW),
+        (MoveAction::Down, KeyCode::KeyS),
+    ]);
+
+    let fire_input_map = InputMap::new([(FireAction, MouseButton::Left)]);
+
+    let mesh = build_mesh(50., 8);
+
+    let player = commands
+        .spawn((
+            Player,
+            ColorMesh2dBundle {
+                mesh: meshes.add(mesh).into(),
+                transform: Transform::from_translation(Vec3::new(0., 0., 0.)),
+                ..Default::default()
+            },
+            Health::from_max(10.),
+            Velocity(Vec3::ZERO),
+            Friction(PLAYER_FRICTION),
+        ))
+        .insert(InputManagerBundle::with_map(move_input_map))
+        .insert(InputManagerBundle::with_map(fire_input_map))
+        .id();
+
+    add_health_bar(&mut commands, player, 70.);
+}
+
+const ENEMY_RADIUS: f32 = 40.;
+
+fn spawn_enemy(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>) {
+    let mesh = build_mesh(ENEMY_RADIUS, 4);
+
+    let x = rand::thread_rng().gen_range(-960.0..960.0);
+    let y = rand::thread_rng().gen_range(-540.0..540.0);
+
+    let enemy = commands
+        .spawn((
+            Enemy,
+            ColorMesh2dBundle {
+                mesh: meshes.add(mesh).into(),
+                transform: Transform::from_xyz(x, y, 0.),
+                ..Default::default()
+            },
+            Health::from_max(3.),
+        ))
+        .id();
+
+    add_health_bar(&mut commands, enemy, 70.);
+}
+
+fn add_health_bar(commands: &mut Commands, entity: Entity, y: f32) {
+    commands.entity(entity).with_children(|parent| {
+        parent.spawn(SpriteBundle {
+            sprite: Sprite {
+                color: Color::BLACK,
+                custom_size: Some(Vec2::new(100.0, 20.0)),
+                ..default()
+            },
+            transform: Transform::from_xyz(0.0, y, 0.0),
+            ..default()
+        });
+
+        parent.spawn((
+            HealthBar,
+            SpriteBundle {
+                sprite: Sprite {
+                    color: Color::WHITE,
+                    custom_size: Some(Vec2::new(75.0, 20.0)),
+                    ..default()
+                },
+                transform: Transform::from_xyz(-50.0, y, 1.0),
+                ..default()
+            },
+        ));
     });
 }
 
+fn update_health_bar(
+    health: Query<(&Health, &Children)>,
+    mut health_bars: Query<(&mut Transform, &mut Sprite), With<HealthBar>>,
+) {
+    for (health, children) in health.iter() {
+        for child in children.iter() {
+            if let Ok((mut transform, mut sprite)) = health_bars.get_mut(*child) {
+                let health_percentage = health.current / health.max;
+                sprite.custom_size = Some(Vec2::new(100.0 * health_percentage, 20.0));
+                transform.translation.x = -50.0 + (50.0 * health_percentage);
+            }
+        }
+    }
+}
+
+fn build_bullet_mesh(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>) {
+    let mesh = meshes.add(build_mesh(10., 5));
+    commands.insert_resource(BulletMesh(mesh.into()));
+}
+
+const PLAYER_MAX_SPEED: f32 = 1000.;
+const PLAYER_SPEED: f32 = 1200.;
+const PLAYER_FRICTION: f32 = 10000.;
+
+fn move_player(mut player: Query<(&mut Velocity, &ActionState<MoveAction>), With<Player>>) {
+    let Ok((mut velocity, action)) = player.get_single_mut() else {
+        return;
+    };
+
+    for action in action.get_pressed() {
+        velocity.add_velocity_clamped(
+            match action {
+                MoveAction::Left => Vec3::ZERO.with_x(-1. * PLAYER_SPEED),
+                MoveAction::Right => Vec3::ZERO.with_x(1. * PLAYER_SPEED),
+                MoveAction::Up => Vec3::ZERO.with_y(1. * PLAYER_SPEED),
+                MoveAction::Down => Vec3::ZERO.with_y(-1. * PLAYER_SPEED),
+            },
+            PLAYER_MAX_SPEED,
+        );
+    }
+
+    // if action.get_pressed().is_empty() {
+    //     velocity.0 = Vec3::ZERO;
+    // }
+}
+
+const BULLET_SPEED: f32 = 1000.;
+
+// TODO: Pool bullets to avoid lag caused by spawning Mesh2dBundle.
+#[derive(Component)]
+struct Bullet;
+
+fn fire_bullets(
+    mut commands: Commands,
+    mut player: Query<(&Transform, &ActionState<FireAction>, &mut Velocity), With<Player>>,
+    q_window: Query<&Window, With<PrimaryWindow>>,
+    q_camera: Query<(&Camera, &GlobalTransform), With<MainCamera>>,
+    bullet_mesh: Res<BulletMesh>,
+) {
+    let Ok((player_transform, action, mut velocity)) = player.get_single_mut() else {
+        return;
+    };
+
+    let (camera, camera_transform) = q_camera.single();
+    let window = q_window.single();
+
+    if let Some(world_position) = window
+        .cursor_position()
+        .and_then(|cursor| camera.viewport_to_world(camera_transform, cursor))
+        .map(|ray| ray.origin.truncate())
+    {
+        if action.just_pressed(&FireAction) {
+            let mut bullet_velocity =
+                Vec3::new(world_position.x, world_position.y, 0.) - player_transform.translation;
+            bullet_velocity = bullet_velocity.normalize_or_zero() * BULLET_SPEED;
+
+            velocity.0 -= bullet_velocity;
+
+            commands.spawn((
+                ColorMesh2dBundle {
+                    mesh: bullet_mesh.0.clone(),
+                    transform: Transform::from_translation(player_transform.translation)
+                        .with_rotation(Quat::from_rotation_z(PI / 2.0)),
+                    ..Default::default()
+                },
+                Velocity(bullet_velocity),
+                Bullet,
+            ));
+        }
+    }
+}
+
+fn update_velocity(mut entities: Query<(&mut Transform, &Velocity)>, time: Res<Time>) {
+    for (mut transform, velocity) in entities.iter_mut() {
+        transform.translation += velocity.0 * time.delta_seconds();
+    }
+}
+
+fn bullet_hit_enemy(
+    mut commands: Commands,
+    bullets: Query<(Entity, &Transform), With<Bullet>>,
+    mut enemies: Query<(&Transform, &mut Health), With<Enemy>>,
+    mut player_camera: ResMut<PlayerCamera>,
+    time: Res<Time>,
+) {
+    for (enemy, mut health) in enemies.iter_mut() {
+        for (bullet, transform) in bullets.iter() {
+            let dist = enemy.translation.distance(transform.translation);
+            if dist < ENEMY_RADIUS {
+                commands.entity(bullet).despawn();
+                health.current -= 1.;
+
+                if health.current <= 0.01 {
+                    player_camera.push_screen_shake_with(10., 0.2, time.elapsed_seconds());
+                }
+            }
+        }
+    }
+}
+
+fn despawn_with_no_health(
+    mut commands: Commands,
+    entities: Query<(Entity, &Health)>,
+    meshes: ResMut<Assets<Mesh>>,
+) {
+    for (entity, health) in entities.iter() {
+        if health.current <= 0.01 {
+            commands.entity(entity).despawn_recursive();
+            spawn_enemy(commands, meshes);
+            return;
+        }
+    }
+}
+
+#[derive(Resource)]
+struct BulletMesh(Mesh2dHandle);
+
 const PI2: f32 = std::f32::consts::PI * 2.0;
 
-pub fn build_circle(radius: f32, vertices: usize) -> Mesh {
+pub fn build_mesh(radius: f32, vertices: usize) -> Mesh {
     let mut mesh = Mesh::new(
         PrimitiveTopology::TriangleList,
         RenderAssetUsages::RENDER_WORLD,
